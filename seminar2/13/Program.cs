ed9dbf8244c23222fd3c86423f1c97ee782d48bd
Aleksey <shatalov_alekse@mail.ru>
2022-11-04 14:32:53 +0700

    домашнее задание семинар 2

---
+++ seminar2/13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n > 99){
+   Console.WriteLine(n>99 ? n.ToString()[2] : '-');
+}
+else if (n < -99){
+   Console.WriteLine(n<-99 ? n.ToString()[3] : '-');
+}   
+else{
+   Console.WriteLine("Третьей цифры в данном числе нет");
+}
+
---
+++ seminar2/15/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите номер дня недели от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+
+switch (a)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота - выходной"); break;
+    case 7: Console.WriteLine("Воскресенье - выходной"); break;
+}
+
+if (a == 6)
+{
+    Console.WriteLine("ВЫХОДНОЙ!!!");
+}
+if (a == 7)
+{
+    Console.WriteLine("ВЫХОДНОЙ!!!");
+}
+
