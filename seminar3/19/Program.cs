8b043f0ad63c6caba1f36b8e8cf48c8b47b300b6
Aleksey <shatalov_alekse@mail.ru>
2022-11-07 10:39:49 +0700

    домашнее задание семинар 3

---
+++ seminar3/19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int a = number.Length;
+
+if (a == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - число является палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - число не является палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
---
+++ seminar3/21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+System.Console.WriteLine("Введите координаты точки 1");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int [] array1 = new int [] {x1,y1};
+System.Console.WriteLine("Введите координаты точки 2");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int [] array2 = new int [] {x2,y2};
+System.Console.WriteLine("Введите координаты точки 3");
+int x3 = int.Parse(Console.ReadLine());
+int y3 = int.Parse(Console.ReadLine());
+int [] array3 = new int [] {x3,y3};
+
+
+
+double result = Math.Cbrt(Math.Pow (array1[0] - array2[0],2) 
+                + Math.Pow (array1[1] - array2[1],2) 
+                + Math.Pow (array3[0] - array3[0],2));
+
+System.Console.WriteLine($"Result is {result}");
+
