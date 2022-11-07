16200d62c780d7401db759f4c6dfa66c4659facc
Aleksey <shatalov_alekse@mail.ru>
2022-11-08 00:25:40 +0700

    домашнее задание семинар 4

---
+++ seminar4/27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+string str = n.ToString();
+int[] b = new int[str.Length];
+int result = 0;
+for( int i=0; i< str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+     result = result + b[i];
+}
+Console.WriteLine(result);
---
+++ seminar4/29/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Massiv();
+void Massiv()
+{
+    int[] N = new int[8];
+        Console.Write("Массив [");
+    for (int i = 0; i < N.Length; i++)
+    {
+        N[i] = new Random().Next(0,99);
+        Console.Write($"{N[i]} ");
+    }
+    Console.Write("]");
+}
