de787f401dc82c6bb2ab8144477b309fabc90d0f
Aleksey <shatalov_alekse@mail.ru>
2022-11-08 00:34:52 +0700

    домашнее задание семинар 5

---
+++ seminar5/34/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. 
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int size)
+{
+	int[] array = new int[size];
+        Console.Write("Массив [");
+    	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(100, 1000);
+        Console.Write($"{array[i]}  ");
+	}
+    Console.Write("]");
+    return array;
+	}
+
+int HowManyEvenNumbers(int[] array)
+{
+	int evenNumber = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+			if (array [i] % 2 == 0)
+				evenNumber++;
+            else evenNumber = evenNumber;
+	}
+
+	return evenNumber;
+}
+
+
+int[] array = CreateArray(10);
+Console.WriteLine();
+Console.WriteLine($"Количество четных значений: " + (HowManyEvenNumbers(array)));
+
---
+++ seminar5/36/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateArray(int size)
+{
+	int[] array = new int[size];
+        Console.Write("Массив [");
+    	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]}  ");
+	}
+    Console.Write("]");
+    return array;
+	}
+
+int SummElements(int[] array)
+{
+	int sum = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+			if (i % 2 == 1 && i!=0)
+				sum = sum + array[i];
+			else 
+			    sum = sum;
+	}
+
+	return sum;
+}
+
+
+int[] array = CreateArray(5);
+Console.WriteLine();
+Console.WriteLine($"Cумма значений элементов на нечетных позициях: " + (SummElements(array)));
+
